9cc6613ec7e47e2f6934c8307401541d977f3c43
marcelo-gms <smarcello023@gmail.com>
2023-06-09 13:14:58 -0300

    edit text about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <C.TextContainer>
           <h4>Sobre mim</h4>
           <h1>Desenvolvedor front-end React.</h1>
-          <p>Meu nome é Marcelo Gomes, sou formado em análise e desenvolvimento de sistemas, e tenho focado no desenvolvimento web como carreira. tenho uma boa base em <span>Html, Css e Javascritp</span>, atualmente estou me aprofundando no <span>React</span>.</p>
+          <p>Meu nome é Marcelo Gomes, sou formado em análise e desenvolvimento de sistemas, e tenho focado no desenvolvimento web como carreira. tenho uma boa base em <span>Html, Css e Javascript</span>, atualmente estou me aprofundando no <span>React</span>.</p>
           <p>estudo <span>node</span> e o banco de dados <span>MongoDB</span> nas horas vagas também.</p>
         </C.TextContainer>
       </C.CenterContainer>
